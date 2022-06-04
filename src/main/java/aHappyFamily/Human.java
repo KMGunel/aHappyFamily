e359bc09629627f947b0a918515c209ac520c5eb
KMGunel <89314536+KMGunel@users.noreply.github.com>
2022-06-04 17:11:32 +0400

    readme-

--- src/main/java/aHappyFamily/Human.java
+++ src/main/java/aHappyFamily/Human.java
@@ -1,5 +1,6 @@
 package main.java.aHappyFamily;
-
+import java.util.Random;
+import java.util.Scanner;
 public class Human {
 
     public String name;
@@ -9,54 +10,62 @@ public class Human {
     public Pet pet;
     public Human mother;
     public Human father;
-    public String [][] schedule;
+    public String[][] schedule;
 
-    public Human(){
+    public Human() {
 
     }
-    public Human(String name,String surname,int year){
-        this.name=name;
-        this.surname=surname;
-        this.year=year;
+
+    public Human(String name, String surname, int year) {
+        this.name = name;
+        this.surname = surname;
+        this.year = year;
     }
 
-    public Human(String name,String surname,int year,Human father,Human mother){
-        this.name=name;
-        this.surname=surname;
-        this.year=year;
-        this.father=father;
-        this.mother=mother;
+    public Human(String name, String surname, int year, Human father, Human mother) {
+        this.name = name;
+        this.surname = surname;
+        this.year = year;
+        this.father = father;
+        this.mother = mother;
     }
 
-    public Human(String name,String surname,int year,byte iq, Pet pet,Human father,Human mother,String[][] schedule){
-        this.name=name;
-        this.surname=surname;
-        this.year=year;
-        this.iq=iq;
-        this.pet=pet;
-        this.father=father;
-        this.mother=mother;
-        this.schedule=schedule;
+    public Human(String name, String surname, int year, byte iq, Pet pet, Human father, Human mother, String[][] schedule) {
+        this.name = name;
+        this.surname = surname;
+        this.year = year;
+        this.iq = iq;
+        this.pet = pet;
+        this.father = father;
+        this.mother = mother;
+        this.schedule = schedule;
     }
 
-
-
-
-    public void greetPet(){
-        System.out.printf("Hello, %s",pet.nickname);
+    public void greetPet() {
+        System.out.printf("Hello, %s", pet.nickname);
     }
-    public void describePet(){
-        if(pet.trickLevel>50){
+
+    public void describePet() {
+        if (pet.trickLevel > 50) {
             pet.str = "very sly";
-        }
-        else{
+        } else {
             pet.str = "almost not sly";
         }
-        System.out.printf("I have a %s, he is %s years old, he is %s",pet.species,pet.age,pet.str);
+        System.out.printf("I have a %s, he is %s years old, he is %s", pet.species, pet.age, pet.str);
     }
 
+    public static void feedPet() {
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+        int randomNum = rand.nextInt(101);
+        byte trickLevel = sc.nextByte();
+        if (trickLevel > randomNum) {
 
+            System.out.println("Hm... I will feed Jack");
+        } else {
 
+            System.out.println("I think Jack is not hungry.");
+        }
 
-
+    }
 }
--- src/main/java/aHappyFamily/Main.java
+++ src/main/java/aHappyFamily/Main.java
@@ -1,9 +1,16 @@
 package main.java.aHappyFamily;
 
+
+
 public class Main {
     public static void main(String[] args) {
 
-        Human boy = new Human("Orxan","Mustafa",(int)2017);
+        Human boy = new Human("Orkhan","Mustafa",(int)2017);
         System.out.println(boy.name);
+
+        System.out.println("Isn't it time for feeding?");
+        System.out.println("TrickLevel?");
+        Human.feedPet();
+
     }
 }
